45ad7f23c1cc494deaa8df2cf1899f801c6d37c9
deolorensa <lorensadeo10@gmail.com>
2022-06-22 22:51:54 +0700

    finish

---
+++ src/views/AboutView.vue
@@ -0,0 +1,24 @@
+<template>
+  <ul v-if="response && response.items.length == 2">
+    <li v-for="item in response.items" :key="item.item.id">
+      {{ item.name }} {{ item.price }}
+    </li>
+  </ul>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      response: null
+    }
+  },
+  mounted() {
+    axios.get('http://localhost:3000/sample-api').then(res => {
+      this.response = res.data
+    })
+  }
+}
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,26 @@
+<template>
+  <input v-model.number="age" type="text" value="number" v-int/>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+     age: 0
+    }
+  },
+  watch: {
+    age(value) {
+      if (value === '') {
+        return
+        }
+          if (typeof value !== 'number') {
+          throw new Error('invalid data type')
+          }
+          if (value < 1) {
+          throw new Error('invalid age')
+        }
+      }
+    }
+  }
+</script>
